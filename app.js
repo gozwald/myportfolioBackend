74a3e2afbb0ce65592134e9f7fe00688e7175911
Samuel Greenwald <gozwald@gmail.com>
2020-07-28 14:46:34 +0200

    mailworking

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 require("dotenv").config();
+var cors = require("cors");
 
 var indexRouter = require("./routes/index");
 var emailRouter = require("./routes/email");
@@ -13,6 +14,7 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/email.js
+++ routes/email.js
@@ -2,7 +2,8 @@ var express = require("express");
 var router = express.Router();
 const nodemailer = require("nodemailer");
 
-router.get("/", function (req, res, next) {
+router.post("/", function (req, res, next) {
+  const { name, email, message } = req.body;
   const transporter = nodemailer.createTransport({
     service: process.env.SERVICE,
     auth: {
@@ -12,15 +13,20 @@ router.get("/", function (req, res, next) {
   });
 
   const mailOptions = {
-    subject: "Invoices due",
-    text: "Dudes, we really need your money",
+    from: process.env.EMAIL,
+    to: process.env.EMAIL,
+    replyTo: email,
+    subject: "New Message from " + name,
+    text: message,
   };
 
   transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
       console.log(error);
+      res.json("bad work sir");
     } else {
       console.log("Email sent: " + info.response);
+      res.json("nice work sir");
     }
   });
 });
