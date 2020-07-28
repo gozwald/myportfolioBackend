be72b492dfb8049be1c349d0904c65a297dc8af7
Samuel Greenwald <gozwald@gmail.com>
2020-07-28 12:38:31 +0200

    first commit

---
+++ routes/email.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var router = express.Router();
+const nodemailer = require("nodemailer");
+
+router.get("/", function (req, res, next) {
+  const transporter = nodemailer.createTransport({
+    service: process.env.SERVICE,
+    auth: {
+      user: process.env.USER,
+      pass: process.env.PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: "gozwald@gmail.com",
+    to: "gozwald@gmail.com",
+    replyTo: "blaaab@blabla.com",
+    subject: "Invoices due",
+    text: "Dudes, we really need your money",
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+});
+
+module.exports = router;
