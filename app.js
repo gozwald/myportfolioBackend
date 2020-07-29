a5af596d0c8bb677c3ba0bcddb9582cc792252f6
Samuel Greenwald <gozwald@gmail.com>
2020-07-29 10:50:36 +0200

    try again cors

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ const corsOptions = {
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
-app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
