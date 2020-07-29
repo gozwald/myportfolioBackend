67e0743af20faf032c6134614279a8300810fb59
Samuel Greenwald <gozwald@gmail.com>
2020-07-29 10:46:25 +0200

    testing origins

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ var emailRouter = require("./routes/email");
 
 var app = express();
 
+const corsOptions = {
+  origin: "https://gallant-curie-314d7c.netlify.app/",
+};
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
@@ -22,7 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/email", emailRouter);
+app.use("/email", cors(corsOptions), emailRouter);
 
 // catch 404 and forward to error hand
 app.use(function (req, res, next) {
